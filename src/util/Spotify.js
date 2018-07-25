4ee19adf7053625a946add3528241697fd0c05ff
ashaney <ashaney@users.noreply.github.com>
2018-07-25 13:35:04 -0500

    jammming submission

---
+++ src/util/Spotify.js
@@ -0,0 +1,77 @@
+// is there a way to load clientID from an external file?
+const clientID = 'f166d67d2200483bb2168653868c5657'
+const redirectURI = 'https://super-jam-place.surge.sh';
+const searchBase = 'https://api.spotify.com/v1/';
+const accessBase = 'https://accounts.spotify.com/authorize';
+
+let accessToken;
+
+const Spotify = {
+  getAccessToken() {
+    if (accessToken) {
+      return accessToken;
+    }
+
+    var accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    var expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+    if (accessTokenMatch && expiresInMatch) {
+      accessToken = accessTokenMatch[1];
+      var expirationTime = Number(expiresInMatch[1]);
+      window.setTimeout(() => accessToken = '', expirationTime * 1000);
+      window.history.pushState('Access Token', null, '/');
+      return accessToken;
+    } else {
+      window.location = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
+    }
+  },
+
+  search(term) {
+    var accessToken = Spotify.getAccessToken();
+    return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+      headers: {Authorization: `Bearer ${accessToken}`}
+    }).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      if (!jsonResponse.tracks) {
+        return [];
+      }
+      return jsonResponse.tracks.items.map(track => ({
+        id: track.id,
+        name: track.name,
+        artist: track.artists[0].name,
+        album: track.album.name,
+        uri: track.uri
+      }));
+    });
+  },
+
+  savePlaylist(playlistName,trackURIs) {
+    if (!playlistName || !trackURIs) {
+      return;
+    }
+    var accessToken = Spotify.getAccessToken();
+    var headers = {Authorization: `Bearer ${accessToken}`};
+    var userID;
+    return fetch('https://api.spotify.com/v1/me', {headers: headers}
+    ).then(response => response.json()
+    ).then(jsonResponse => {
+      userID = jsonResponse.id;
+      return fetch(`https://api.spotify.com/v1/users/${userID}/playlists`, {
+        headers: headers,
+        method: 'POST',
+        body: JSON.stringify({name: playlistName})
+      }).then(response => response.json()
+      ).then(jsonResponse => {
+        const playlistID = jsonResponse.id;
+        return fetch(`https://api.spotify.com/v1/users/${userID}/playlists/${playlistID}/tracks`, {
+          headers: headers,
+          method: 'POST',
+          body: JSON.stringify({uris: trackURIs})
+        });
+      });
+    });
+  }
+};
+
+export default Spotify;
